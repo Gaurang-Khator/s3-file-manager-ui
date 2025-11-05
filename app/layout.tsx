8669ae54cf0a7a5826886beb40fdde367a5a84f2
Gaurang Khator <gaurangkhator04@gmail.com>
2025-11-05 15:19:08 +0530

    added auth

---
+++ app/layout.tsx
@@ -0,0 +1,43 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+import { ClerkProvider, SignedOut, SignIn, SignedIn } from "@clerk/nextjs";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "S3 File Manager UI",
+  description: "Created by Gaurang Khator",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <SignedOut>
+          <div className="min-h-screen min-w-screen flex justify-center items-center">
+            <SignIn routing="hash"/>
+            </div>
+        </SignedOut>
+        <SignedIn> {children} </SignedIn>
+      </body>
+    </html>
+    </ClerkProvider>
+  );
+}
---
+++ components/ui/nav.tsx
@@ -0,0 +1,17 @@
+'use cleint';
+
+import { UserButton } from "@clerk/nextjs";
+import * as React from "react";
+
+const NavBar: React.FC = () => {
+    return <nav className="p-4 flex justify-between items-center">
+        <div>
+            <h1 className="font-bold">S3 File Manager UI</h1>
+        </div>
+        <div className="p-4">
+            <UserButton />
+        </div>
+    </nav>
+};
+
+export default NavBar;
